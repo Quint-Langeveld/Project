8a0ae67f573017a8d5398baa7a64767a6e3bac66
Quint-Langeveld <quintlangeveld@hotmail.com>
2019-01-10 16:09:53 +0100

    progress code commit

--- app/src/main/java/com/example/qlangeveld/testit/MainActivity.java
+++ app/src/main/java/com/example/qlangeveld/testit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.qlangeveld.testit;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle("by Quint");
+        }
 
 
     // BRONNNN
