1e0046a2e199f647a0725d897b6ddebb8d2fe4b8
Quint-Langeveld <quintlangeveld@hotmail.com>
2019-01-30 12:01:19 +0100

    color of buttons in Mainactivity change added

--- app/src/main/java/com/example/qlangeveld/testit/MainActivity.java
+++ app/src/main/java/com/example/qlangeveld/testit/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.qlangeveld.testit;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +14,7 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -31,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button finished = findViewById(R.id.finishedButton);
+        finished.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.littleMoreBlue)));
+
 
         db = EntryDatabase.getInstance(this);
         Cursor curs = db.selectOngoing();
@@ -171,6 +180,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     public void onGoingClicked() {
+
+        Button finished = findViewById(R.id.finishedButton);
+        Button ongoing = findViewById(R.id.ongoingButton);
+        finished.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.littleMoreBlue)));
+        ongoing.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.tintedBlue)));
+
+
         Cursor curs = db.selectOngoing();
         entryAdapter = new EntryAdapter(this, curs);
 
@@ -180,6 +196,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     public void onFinishedClicked() {
+
+        Button ongoing = findViewById(R.id.ongoingButton);
+        Button finished = findViewById(R.id.finishedButton);
+        ongoing.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.littleMoreBlue)));
+        finished.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.tintedBlue)));
+
         Cursor curs = db.selectFinished();
         finishedAdapter = new FinishedAdapter(this, curs);
 
